adbb3e6c7e76056afb330463f9266cda3661619a
kai63001 <animehq5@gmail.com>
2021-11-04 15:49:32 +0700

    update simple mint

---
+++ contracts/SimpleMint.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+
+contract TestNft is ERC721URIStorage {
+    using Counters for Counters.Counter;
+   Counters.Counter private _tokenIds;
+
+    constructor () ERC721 ("Romeo","RO") {
+  
+    }
+    
+   function mintNFT(string memory tokenURI) public returns (uint256) {
+       _tokenIds.increment();
+       uint256 newItemId = _tokenIds.current();
+       _safeMint(msg.sender, newItemId);
+       _setTokenURI(newItemId, tokenURI);
+       return newItemId;
+   }
+}
