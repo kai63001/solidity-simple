056dff436947d428e2cf54625661a1452f01e4f1
kai63001 <animehq5@gmail.com>
2021-11-04 19:51:04 +0700

    hahahaaha

--- contracts/SimpleMint.sol
+++ contracts/SimpleMint.sol
@@ -8,9 +8,12 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract TestNft is ERC721URIStorage {
     using Counters for Counters.Counter;
    Counters.Counter private _tokenIds;
+   uint public deposits = 0;
+   uint amount = 0;
+    address payable public Owner;
 
     constructor () ERC721 ("Romeo","RO") {
-  
+        Owner = payable(msg.sender); 
     }
     
    function mintNFT(string memory tokenURI) public returns (uint256) {
@@ -20,8 +23,27 @@ contract TestNft is ERC721URIStorage {
        _setTokenURI(newItemId, tokenURI);
        return newItemId;
    }
-
+   
    function totalSupply() public view returns (Counters.Counter memory) {
        return _tokenIds;
    }
+   
+   modifier onlyOwner() {
+        require(msg.sender == Owner, 'Not owner');
+        _;
+    }
+   
+   function deposit() public payable returns (uint256) {
+      deposits= 1;
+      amount += msg.value;
+      return msg.value;
+    }
+    
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    } 
+    
+    function withdraw (uint _amount) public onlyOwner {
+        Owner.transfer(_amount); 
+    }
 }
